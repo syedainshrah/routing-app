c7d1f7606d0af3aee8bed08cc9c6491354383bcc
Syedainshrah <161240095+syedainshrah@users.noreply.github.com>
2024-12-13 00:40:38 +0500

    Resolve merge conflicts

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -29,23 +29,23 @@ export default function Header() {
                 {/* Links Section - Mobile Menu */}
                 {isMenuOpen && (
                     <div className="md:hidden absolute top-full left-0 w-full bg-yellow-200 flex flex-col items-center space-y-4 py-6 shadow-lg">
-                        <Link href="/" className="hover:text-gray-700" onClick={toggleMenu}>Home</Link>
-                        <Link href="/pages/shop" className="hover:text-gray-700" onClick={toggleMenu}>Shop</Link>
-                        <Link href="/About" className="hover:text-gray-700" onClick={toggleMenu}>About</Link>
-                        <Link href="/pages/contact" className="hover:text-gray-700" onClick={toggleMenu}>Contact</Link>
+                        <Link href="/" className="text-black hover:text-gray-300" onClick={toggleMenu}>Home</Link>
+                        <Link href="/pages/shop" className="text-black hover:text-gray-700" onClick={toggleMenu}>Shop</Link>
+                        <Link href="/About" className="hover:text-gray-700 text-black " onClick={toggleMenu}>About</Link>
+                        <Link href="/pages/contact" className="hover:text-gray-700 text-black" onClick={toggleMenu}>Contact</Link>
                     </div>
                 )}
 
                 {/* Desktop Links Section */}
                 <div className="hidden md:flex justify-center items-center gap-8 lg:gap-16 flex-grow">
-                    <Link href="/" className="hover:text-gray-700">Home</Link>
-                    <Link href="/pages/shop" className="hover:text-gray-700">Shop</Link>
-                    <Link href="/About" className="hover:text-gray-700">About</Link>
-                    <Link href="/pages/contact" className="hover:text-gray-700">Contact</Link>
+                    <Link href="/" className= "text-black hover:text-gray-700">Home</Link>
+                    <Link href="/pages/shop" className="text-black hover:text-gray-700">Shop</Link>
+                    <Link href="/About" className="hover:text-gray-700 text-black">About</Link>
+                    <Link href="/pages/contact" className="hover:text-gray-700 text-black">Contact</Link>
                 </div>
 
                 {/* Icons Section */}
-                <div className="flex items-center gap-4 sm:gap-6">
+                <div className="flex items-center gap-4 sm:gap-6 text-black">
                     {/* Contact Icon */}
                     <Link href="/contact" className="hover:text-gray-700">
                         <IoMdContact className="text-xl" />
